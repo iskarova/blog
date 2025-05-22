03da65c0c4ef30eb59d2656b888e14d869874cf0
lina iskarova <cyberdeadushka@gmail.com>
2025-05-22 16:35:55 +0300

    update view for editing blog post

--- blog/views.py
+++ blog/views.py
@@ -28,3 +28,16 @@ def post_new(request):
   return render(request, 'blog/post_edit.html', {'form': form})
 
 
+def post_edit(request, pk):
+  post = Post.objects.get(pk=pk)
+  if request.method == "POST":
+    form = PostForm(request.POST, instance=post)
+    if form.is_valid():
+      post = form.save(commit=False)
+      post.author = request.user
+      post.published_date = timezone.now()
+      post.save()
+      return redirect('post_detail', pk=post.pk)
+  else:
+    form = PostForm(instance=post)
+  return render(request, 'blog/post_edit.html', {'form': form})
